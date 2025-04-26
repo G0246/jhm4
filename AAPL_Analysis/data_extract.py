7ae9059e4beecf389796878fda1c563f2699c706
Veutexus <70145360+G0246@users.noreply.github.com>
2025-04-26 05:01:14 +0000

    analysis

--- AAPL_Analysis/data_extract.py
+++ AAPL_Analysis/data_extract.py
@@ -6,5 +6,5 @@ from datetime import datetime, timedelta
 
 ticker_symbol = 'AAPL'
 
-data = yf.download(ticker_symbol, start='2020-01-01', end='2024-04-25')
+data = yf.download(ticker_symbol, start='2020-01-01', end='2025-04-25')
 data.to_csv('AAPL_historical_data.csv')
